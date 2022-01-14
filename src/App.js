2318d6ec4e5b2e7db931a007e96e090170cf4222
hellodeborahuk <29425781+hellodeborahuk@users.noreply.github.com>
2022-01-14 10:39:04 +0000

    Moved nav and footer to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import './App.css';
-import Nav from "./components/Nav"
-import Hero from "./components/Hero"
-import Info from "./components/Info"
-import ClassInfo from "./components/ClassInfo"
-import MailingList from "./components/MailingList"
-import LatestBlog from "./components/LatestBlog"
-import Gallery from "./components/Gallery"
-import Footer from "./components/Footer"
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Hero from "./components/Hero";
+import Info from "./components/Info";
+import ClassInfo from "./components/ClassInfo";
+import MailingList from "./components/MailingList";
+import LatestBlog from "./components/LatestBlog";
+import Gallery from "./components/Gallery";
+import {FaFacebookSquare, FaInstagram} from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Hero />
-      <Info />
-      <ClassInfo />
-      <MailingList />
-      <LatestBlog />
-      <Gallery />
-      <Footer />
+      <Router>
+        <nav>
+          <div className="left-nav">
+            <a href="/">Home</a>
+            <a href="/">Blog</a>
+            <a href="/">Contact</a>
+          </div>
+          <h1>Debbie Dann Yoga</h1>
+          <div className="right-nav">
+            <button>Book your class</button>
+          </div>
+        </nav>
+        <Hero />
+        <Info />
+        <ClassInfo />
+        <MailingList />
+        <LatestBlog />
+        <Gallery />
+        <footer>
+          <div>
+            <p>
+              Hi, I’m Debbie! I am a proud mum of two girls and live in Andover,
+              Hampshire. I completed my Yoga Alliance Professional accredited
+              RYT 200hrs teacher training with Jill Jones Yoga. Doing yoga and
+              meditation keeps me calm and helps me cope with daily family life.
+            </p>
+          </div>
+          <div className="social">
+            <a href="/" className="social-icons instagram">
+              <FaInstagram />
+            </a>
+            <a href="/" className="social-icons facebook">
+              <FaFacebookSquare />
+            </a>
+            <img
+              src="./images/logo-nameless-small.png"
+              alt="logo"
+              className="logo"
+            />
+          </div>
+          <div className="footer-nav">
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <a href="/">Privacy Policy</a>
+            <a href="/">Terms and Conditions</a>
+          </div>
+        </footer>{" "}
+      </Router>
     </div>
   );
 }
